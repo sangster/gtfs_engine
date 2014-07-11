a4246399802d3ca2544732e40c7400925f8d7da8
Jon Sangster <jon@ertt.ca>
2014-07-11 02:47:42 -0300

    Reorder routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,20 @@ GtfsEngine::Engine.routes.draw do
 
     r.resources :data_sets, path: '' do
       r.resources :agencies
+      r.resources :stops
+      r.resources :routes
+      r.resources :trips, only: [:show] do
+        collection { get 'block/:id', action: :block }
+      end
+      r.resources :stop_times
       r.resources :calendars do
         member { get :dates }
       end
       r.resources :calendar_dates
       r.resources :fare_attributes
       r.resources :fare_rules
-      r.resources :frequencies
-      r.resources :routes
       r.resources :shapes, only: [:show]
-      r.resources :stops
-      r.resources :stop_times
+      r.resources :frequencies
       r.resources :transfers, only: [:index] do
         collection do
           get 'from/:stop_id', action: :from
@@ -21,10 +24,7 @@ GtfsEngine::Engine.routes.draw do
           get 'from/:from_id/to/:to_id', action: :from_to
         end
       end
-      r.resources :trips, only: [:show] do
-        collection { get 'block/:id', action: :block }
-      end
+      r.resources :feed_infos, only: [:index]
     end
-
   end
 end
