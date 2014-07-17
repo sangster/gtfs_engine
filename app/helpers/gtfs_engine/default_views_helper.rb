4421870155f9c69bf71f303084fbc0cd8484b9f4
Jon Sangster <jon@ertt.ca>
2014-07-17 04:09:39 -0300

    Revert "Extract helpers for clients to use"
    This reverts commit 936c457034a2c99e39c959064aac88c64dbb843a.
    
    Conflicts:
    	gtfs-engine.gemspec

--- app/helpers/gtfs_engine/default_views_helper.rb
+++ app/helpers/gtfs_engine/default_views_helper.rb
@@ -1,5 +1,28 @@
 module GtfsEngine
   module DefaultViewsHelper
-    include GtfsViewsHelper
+    def index(json, records)
+      json.cache! [controller_name, filter] do
+        json.ignore_nil! true
+
+        json.status 'success'
+        json.data do
+          json.set! controller_name do
+            json.array!(records) {|record| json.extract! record, *fields }
+          end
+        end
+      end
+    end
+
+    def show(json, record)
+      json.cache! record do
+        json.ignore_nil! true
+        json.status 'success'
+        json.data do
+          json.set! controller_name.singularize do
+            json.extract! record, *fields
+          end
+        end
+      end
+    end
   end
 end
--- lib/gtfs_engine.rb
+++ lib/gtfs_engine.rb
@@ -2,7 +2,6 @@ require 'gtfs_reader'
 
 require 'gtfs_engine/engine'
 require 'gtfs_engine/exceptions'
-require 'gtfs_engine/gtfs_views_helper'
 require 'gtfs_engine/json_responder'
 require 'gtfs_engine/sources'
 
